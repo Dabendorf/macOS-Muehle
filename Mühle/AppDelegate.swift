cf93b36b478a6f452acd054b1a28027d9eb23bcc
Lukas Schramm <schramm98@icloud.com>
2016-07-30 09:32:19 +0200

    Initial Commit

---
+++ Mühle/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mühle
+//
+//  Created by Lukas Schramm on 16-07-30.
+//  Copyright © 2016 DOR. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mühle/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mühle
+//
+//  Created by Lukas Schramm on 16-07-30.
+//  Copyright © 2016 DOR. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
